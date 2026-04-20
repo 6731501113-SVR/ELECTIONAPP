8d6d028457b843c1b51bedec987cb0fe371a50a5
i <stu43677@samakkhi.ac.th>
2026-04-20 11:29:59 +0700

    dbip

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 const con = mysql.createPool({
-    host: '192.168.1.122', // check the port
+    host: '192.168.1.146', // check the port
     user: 'test', // user = test
     password: '1234', // pass = 1234
     database: 'election',
